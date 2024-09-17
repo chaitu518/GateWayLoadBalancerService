37a14bdc25d9a2e310541f524d931b0d50c47b73
saichaitanya518 <saichaitanya518@gmail.com>
2024-09-17 23:33:57 +0530

    first commit

---
+++ src/main/java/com/example/gatewayloadbalancerservice/GateWayLoadBalancerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.gatewayloadbalancerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateWayLoadBalancerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateWayLoadBalancerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gatewayloadbalancerservice/GateWayLoadBalancerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatewayloadbalancerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateWayLoadBalancerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
